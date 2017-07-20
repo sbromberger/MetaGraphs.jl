dc19d75ee09cf2f0312883715cf9354740bbfd1f
Seth Bromberger <sbromberger@users.noreply.github.com>
2017-07-19 21:10:22 -0700

    Revert "docfix"

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,19 @@
 using Documenter
 include("../src/MetaGraphs.jl")
+using MetaGraphs
 
+# index is equal to the README for the time being
 cp(normpath(@__FILE__, "../../README.md"), normpath(@__FILE__, "../src/index.md"); remove_destination=true)
 
+# same for license
 cp(normpath(@__FILE__, "../../LICENSE.md"), normpath(@__FILE__, "../src/license.md"); remove_destination=true)
 
-makedocs(
-    sitename = "MetaGraphs.jl",
-    modules = [MetaGraphs],
-    format = :html,
-    clean = false,
-    pages = Any["Home" => "index.md"],
-)
+makedocs(modules=[MetaGraphs], doctest = true)
+
 
 deploydocs(
-    target = "build",
     deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-    make = nothing,
-    repo = "github.com/JuliaGraphs/MetaGraphs.jl.git",
+    repo   = "github.com/JuliaGraphs/MetaGraphs.jl.git",
     julia  = "0.6"
 )
 
